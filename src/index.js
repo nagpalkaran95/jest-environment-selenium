c9c9fe0818f6317ef877a78db6d8c025da9425da
Karan Nagpal <karannagpal@Karans-MacBook-Pro.local>
2022-04-28 12:23:27 +0530

    looping

--- src/index.js
+++ src/index.js
@@ -5,20 +5,28 @@ import proxy from 'selenium-webdriver/proxy';
 class WebdriverEnvironment extends NodeEnvironment {
   constructor(config) {
     super(config);
-    this.configuration = Object.assign(
-      {
-        capabilities: {
-          browserName: 'chrome'
-        }
-      },
-      config.testEnvironmentOptions
-    );
+    config.testEnvironmentOptions = config.projectConfig.testEnvironmentOptions;
+    // this.configuration = Object.assign(
+    //   {
+    //     capabilities: {
+    //       browserName: 'chrome'
+    //     }
+    //   },
+    //   config.testEnvironmentOptions
+    // );
+
+    this.configuration = config.testEnvironmentOptions;
     this.global.webdriver = webdriver;
     this.global.By = webdriver.By;
     this.global.until = webdriver.until;
     this.global.configuration = this.configuration;
     this.global.cleanup = async () => {
-      await this.global.driver.quit();
+      // await this.global.driver.quit();
+      asyncFunctions = [];
+      this.global.driver.forEach(_dr => {
+        asyncFunctions.push(_dr.quit());
+      })
+      await Promise.all(asyncFunctions);
       this.global.driver = await buildDriver(this.configuration);
     };
   }
@@ -30,7 +38,12 @@ class WebdriverEnvironment extends NodeEnvironment {
 
   async teardown() {
     if (this.global.driver) {
-      await this.global.driver.quit();
+      // await this.global.driver.quit();
+      asyncFunctions = [];
+      this.global.driver.forEach(_dr => {
+        asyncFunctions.push(_dr.quit());
+      })
+      await Promise.all(asyncFunctions);
     }
     await super.teardown();
   }
@@ -41,20 +54,25 @@ class WebdriverEnvironment extends NodeEnvironment {
 }
 
 async function buildDriver(configuration) {
-  const driver = new webdriver.Builder().withCapabilities(configuration.capabilities);
+  let drivers = [];
+  configuration.capabilities.forEach(caps => {
+    let driver = new webdriver.Builder().withCapabilities(caps);
 
-  if (configuration.server) driver.usingServer(configuration.server);
-  if (configuration.proxyType) {
-    let prxy;
-    if (configuration.proxyType === 'socks') {
-      prxy = proxy.socks(configuration.proxyOptions.socksProxy, configuration.proxyOptions.socksVersion);
-    } else {
-      prxy = proxy[configuration.proxyType](configuration.proxyOptions);
+    if (configuration.server) driver.usingServer(configuration.server);
+    if (configuration.proxyType) {
+      let prxy;
+      if (configuration.proxyType === 'socks') {
+        prxy = proxy.socks(configuration.proxyOptions.socksProxy, configuration.proxyOptions.socksVersion);
+      } else {
+        prxy = proxy[configuration.proxyType](configuration.proxyOptions);
+      }
+      driver.setProxy(prxy);
     }
-    driver.setProxy(prxy);
-  }
 
-  return driver.build();
+    drivers.push(driver);
+    // return driver.build();
+  });
+  return drivers;
 }
 
 module.exports = WebdriverEnvironment;
